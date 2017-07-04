709eee9440f80b742b48ad498270f8a39667f7d4
Gonzo Fialho <carloseaf@gmail.com>
2017-07-04 04:32:52 -0300

    Pca as class

--- pca.py
+++ pca.py
@@ -2,23 +2,35 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import os
 
 
-def pca(A, B=None, numpc=None):
-    A = (A - np.mean(A.T, axis=1)).T
-    latent, coeff = np.linalg.eig(np.cov(A))
-    idx = np.argsort(latent)
-    idx = idx[::-1]
+class PCA:
+    def __init__(self):
+        pass
 
-    coeff = coeff[:, idx]
-    latent = latent[idx]
-    coeff = coeff[:, range(numpc)]
-    A = (coeff.T @ A).T
+    def fit(self, A, numpc):
+        A = (A - np.mean(A.T, axis=1)).T
+        latent, coeff = np.linalg.eig(np.cov(A))
+        idx = np.argsort(latent)
+        idx = idx[::-1]
 
-    if B is not None:
-        B = (B - np.mean(B.T, axis=1)).T
-        B = (coeff.T @ B).T
+        coeff = coeff[:, idx]
+        latent = latent[idx]
+        coeff = coeff[:, range(numpc)]
 
-        return np.real(A), np.real(B)
+        self.latent = latent
+        self.coeff = coeff
 
-    return np.real(A)
+    def transform(self, A):
+        A = (A - np.mean(A.T, axis=1)).T
+        return np.real((self.coeff.T @ A).T)
+
+    def save(self, dirname):
+        os.makedirs(dirname, exist_ok=True)
+        np.save(os.path.join(dirname, 'coeff.npy'), self.coeff)
+        np.save(os.path.join(dirname, 'latent.npy'), self.latent)
+
+    def load(self, dirname):
+        self.coeff = np.load(os.path.join(dirname, 'coeff.npy'))
+        self.latent = np.load(os.path.join(dirname, 'latent.npy'))
